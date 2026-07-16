eb95aaaf6ac687b7d368c3500bb97119b5e0e2da
Husan Eshonqulov <husanswe@gmail.com>
2026-07-16 14:46:01 +0500

    polling to webhook

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
-import { Bot } from "grammy";
+import { Bot, webhookCallback } from "grammy";
 
 const botToken = Deno.env.get("BOT_TOKEN");
 if (botToken === undefined) {
-  throw new Error("Bot token is not provided!");
+  console.error("BOT_TOKEN is not provided!");
+  Deno.exit(1);
 }
 
 const bot = new Bot(botToken);
@@ -10,4 +11,19 @@ const bot = new Bot(botToken);
 bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 bot.on("message", (ctx) => ctx.reply("Got another message!"));
 
-bot.start();
+const handleUpdate = webhookCallback(bot, "std/http");
+
+Deno.serve(async (req) => {
+  if (req.method === "POST") {
+    const url = new URL(req.url);
+    if (url.pathname === "/bot") {
+      try {
+        return await handleUpdate(req);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
+
+  return new Response("Not found", { status: 404 });
+});
